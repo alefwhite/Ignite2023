8be567149c1c1967fdcf278dde68d6c16b174ada
alefwhite <alefwhite@hotmail.com>
2023-08-07 15:52:29 -0300

    feat: finalizando módulo 3 e iniciando módulo 4

--- Node/03-ApiNode-Com-Solid/src/app.ts
+++ Node/03-ApiNode-Com-Solid/src/app.ts
@@ -1,8 +1,27 @@
 import fastify from 'fastify';
-import { PrismaClient } from '@prisma/client';
+import { appRoutes } from '@/http/routes';
+import { ZodError } from 'zod';
+import { env } from '@/env';
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
+app.register(appRoutes);
 
-prisma.$connect();
+app.setErrorHandler((error, _, reply) => {
+    if (error instanceof ZodError) {
+        return reply
+            .status(400)
+            .send({
+                message: 'Validation error.',
+                issues: error.format()
+            });
+    }
+
+    if (env.NODE_ENV !== 'production') {
+        console.error(error);
+    } else {
+        // HERE WE SHOULD LOG TO AN EXTERNAL TOOL LIKE DataDog/NewRelic/Sentry
+    }
+
+    return reply.status(500).send({ message: 'Internal server error.' });
+});
---
+++ Node/03-ApiNode-Com-Solid/src/http/controllers/register.ts
@@ -0,0 +1,30 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
+import { RegisterUseCase } from '@/use-cases/register';
+import { UsersRepository } from '@/repositories/users-repository';
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error';
+export async function register (request: FastifyRequest, reply: FastifyReply)  {
+    const registerBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6)
+    });
+
+    const { name, email, password } = registerBodySchema.parse(request.body);
+
+    try {
+        const usersRepository = new UsersRepository();
+
+        const registerUseCase = new RegisterUseCase(usersRepository);
+
+        await registerUseCase.execute({ name, email, password });
+    } catch (error) {
+        if (error instanceof UserAlreadyExistsError) {
+            return reply.status(409).send({ message: error.message });
+        }
+
+        throw error;
+    }
+
+    return reply.status(201).send();
+}
