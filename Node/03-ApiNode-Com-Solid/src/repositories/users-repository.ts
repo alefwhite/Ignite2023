9407f86b32d9706869d5ff6420b824f3d0140eb0
alefwhite <alefwhite@hotmail.com>
2023-08-09 20:29:11 -0300

    chore: add prettier

--- Node/03-ApiNode-Com-Solid/src/repositories/users-repository.ts
+++ Node/03-ApiNode-Com-Solid/src/repositories/users-repository.ts
@@ -1,25 +1,22 @@
 import { prisma } from '@/lib/prisma';
 import { Prisma } from '@prisma/client';
 import { IUsersRepository } from '@/repositories/contracts/users-repository';
+
 export class UsersRepository implements IUsersRepository {
-    async findByEmail(email: string) {
-        const user = await prisma
-            .user
-            .findUnique({
-                where: {
-                    email
-                }
-            });
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
 
-        return user;
-    }
-    async create (data: Prisma.UserCreateInput) {
-        const user = await prisma.user.create({
-            data
-        });
+    return user;
+  }
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data,
+    });
 
-        return user;
-    }
-
-
+    return user;
+  }
 }
--- Node/03-ApiNode-Com-Solid/src/use-cases/register.spec.ts
+++ Node/03-ApiNode-Com-Solid/src/use-cases/register.spec.ts
@@ -1,5 +1,19 @@
-import { test, expect } from 'vitest';
+import { expect, it } from 'vitest';
+import { UsersRepository } from '@/repositories/users-repository';
+import { RegisterUseCase } from '@/use-cases/register';
 
-test('check if it works', () => {
-    expect(2 + 2).toBe(4);
+// @ts-ignore
+expect('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const usersRepository = new UsersRepository();
+    const registerUseCase = new RegisterUseCase(usersRepository);
+
+    const { user } = await registerUseCase.execute({
+      name: 'Romero',
+      email: 'romero@gmail.com',
+      password: '123456',
+    });
+
+    console.log(user);
+  });
 });
