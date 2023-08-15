a8f3a7ae133bccb3641ce76bbd974467865b9438
alefwhite <alefwhite@hotmail.com>
2023-08-14 23:01:10 -0300

    test: config tests

--- Node/03-ApiNode-Com-Solid/src/use-cases/register.spec.ts
+++ Node/03-ApiNode-Com-Solid/src/use-cases/register.spec.ts
@@ -1,19 +1,62 @@
-import { expect, it } from 'vitest';
-import { UsersRepository } from '@/repositories/users-repository';
-import { RegisterUseCase } from '@/use-cases/register';
+import { describe, expect, it } from 'vitest';
 
+import { RegisterUseCase } from '@/use-cases/register';
+import { compare } from 'bcryptjs';
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository';
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error';
+
+// UNIT TEST
 // @ts-ignore
-expect('Register Use Case', () => {
-  it('should hash user password upon registration', async () => {
-    const usersRepository = new UsersRepository();
+describe('Register Use Case', () => {
+  it('should be able to register', async () => {
+    const usersRepository = new InMemoryUsersRepository();
     const registerUseCase = new RegisterUseCase(usersRepository);
 
     const { user } = await registerUseCase.execute({
-      name: 'Romero',
-      email: 'romero@gmail.com',
+      name: 'Romero Britto',
+      email: 'romero_britto@gmail.com',
       password: '123456',
     });
 
-    console.log(user);
+    expect(user.id).toEqual(expect.any(String));
+  });
+
+  it('should hash user password upon registration', async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterUseCase(usersRepository);
+
+    const { user } = await registerUseCase.execute({
+      name: 'Romero Britto',
+      email: 'romero_britto@gmail.com',
+      password: '123456',
+    });
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    );
+
+    expect(isPasswordCorrectlyHashed).toBe(true);
+  });
+
+  it('should not be able to register with same email twice', async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterUseCase(usersRepository);
+
+    const email = 'romero_britto@gmail.com';
+
+    await registerUseCase.execute({
+      name: 'Romero Britto',
+      email,
+      password: '123456',
+    });
+
+    await expect(() =>
+      registerUseCase.execute({
+        name: 'Romero Britto',
+        email,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError);
   });
 });
