962e2ee0683d9b99c14f13ff69b6431dc7a21409
alefwhite <alefwhite@hotmail.com>
2023-08-23 16:34:03 -0300

    feat: add check ins history

--- Node/03-ApiNode-Com-Solid/src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ Node/03-ApiNode-Com-Solid/src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -5,6 +5,7 @@ import dayjs from 'dayjs';
 
 export class InMemoryCheckInsRepository implements ICheckInsRepository {
   public items: CheckIn[] = [];
+
   async findByUserIdOnDate(userId: string, date: Date) {
     const startOfTheDay = dayjs(date).startOf('date');
 
@@ -24,6 +25,12 @@ export class InMemoryCheckInsRepository implements ICheckInsRepository {
     }
 
     return checkInOnSameDate;
+  }
+
+  async findManyByUserId(userId: string, page: number) {
+    return this.items
+      .filter(item => item.user_id === userId)
+      .slice((page - 1) * 20, page * 20);
   }
 
   async create(data: Prisma.CheckInUncheckedCreateInput) {
