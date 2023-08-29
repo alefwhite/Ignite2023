f7748d56255eee73e2adeb4f63f1524a37d503dc
alefwhite <alefwhite@hotmail.com>
2023-08-29 00:00:58 -0300

    feat: implementation jwt

--- Node/03-ApiNode-Com-Solid/src/app.ts
+++ Node/03-ApiNode-Com-Solid/src/app.ts
@@ -2,26 +2,29 @@ import fastify from 'fastify';
 import { appRoutes } from '@/http/routes';
 import { ZodError } from 'zod';
 import { env } from '@/env';
+import fastifyJwt from '@fastify/jwt';
 
 export const app = fastify();
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+});
 
 app.register(appRoutes);
 
 app.setErrorHandler((error, _, reply) => {
-    if (error instanceof ZodError) {
-        return reply
-            .status(400)
-            .send({
-                message: 'Validation error.',
-                issues: error.format()
-            });
-    }
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      issues: error.format(),
+    });
+  }
 
-    if (env.NODE_ENV !== 'production') {
-        console.error(error);
-    } else {
-        // HERE WE SHOULD LOG TO AN EXTERNAL TOOL LIKE DataDog/NewRelic/Sentry
-    }
+  if (env.NODE_ENV !== 'production') {
+    console.error(error);
+  } else {
+    // HERE WE SHOULD LOG TO AN EXTERNAL TOOL LIKE DataDog/NewRelic/Sentry
+  }
 
-    return reply.status(500).send({ message: 'Internal server error.' });
+  return reply.status(500).send({ message: 'Internal server error.' });
 });
