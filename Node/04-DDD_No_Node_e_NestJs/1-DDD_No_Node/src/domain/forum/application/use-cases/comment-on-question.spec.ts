446affe4b198e00b94367635364bfea81b6bebf9
alefwhite <alefwhite@hotmail.com>
2023-09-09 15:49:55 -0300

    feat: delete comments

--- Node/04-DDD_No_Node_e_NestJs/1-DDD_No_Node/src/domain/forum/application/use-cases/comment-on-question.spec.ts
+++ Node/04-DDD_No_Node_e_NestJs/1-DDD_No_Node/src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -1,6 +1,6 @@
 import { InMemoryQuestionsRepository } from '@/test/repositories/in-memory-questions-repository';
 import { makeQuestion } from '@/test/factories/make-question';
-import { InMemoryQuestionCommentsRepository } from '@/test/repositories/in-memory-question-comments';
+import { InMemoryQuestionCommentsRepository } from '@/test/repositories/in-memory-question-comments-repository';
 import { CommentOnQuestionUseCase } from '@/domain/forum/application/use-cases/comment-on-question';
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
