b696037bd9b6cd64e8ff2a05bb5654fc5e51b12b
alefwhite <alefwhite@hotmail.com>
2023-09-18 18:00:38 -0300

    refactor: add watched list

--- Node/04-DDD_No_Node_e_NestJs/1-DDD_No_Node/src/domain/forum/application/use-cases/create-question.ts
+++ Node/04-DDD_No_Node_e_NestJs/1-DDD_No_Node/src/domain/forum/application/use-cases/create-question.ts
@@ -3,6 +3,7 @@ import { Question } from '@/domain/forum/enterprise/entities/question';
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 import { Either, right } from '@/core/either';
 import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment';
+import { QuestionAttachmentList } from '@/domain/forum/enterprise/entities/question-attachment-list';
 
 interface CreateQuestionUseCaseRequest {
   authorId: string;
@@ -39,7 +40,7 @@ export class CreateQuestionUseCase {
       });
     });
 
-    question.attachments = questionAttachments;
+    question.attachments = new QuestionAttachmentList(questionAttachments);
 
     await this.questionRepository.create(question);
 
