7446149b5bd93ba62ca4d92ed00e325b65761571
alefwhite <alefwhite@hotmail.com>
2024-02-19 15:54:59 -0300

    Ok

--- React-Native/imhere/src/screens/Home/index.tsx
+++ React-Native/imhere/src/screens/Home/index.tsx
@@ -1,15 +1,22 @@
+import { useState } from 'react';
 import { Text, TextInput, View, TouchableOpacity, Alert, FlatList } from 'react-native';
 import { styles } from './styles';
 
 import { Participant } from '../../components/Participant';
 
 export function Home() {
-  const participants = ['Alef', 'Samara', 'Théo', 'Amanda', 'Marilene', 'Bete', 'Bill', 'Doris', 'Eric', 'Carol']
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setParticipantName] = useState('');
 
   function handleParticipantAdd() {
-    if (participants.includes("Rodrigo")) {
+    if (participantName.length === 0) return
+    
+    if (participants.includes(participantName)) {
       return Alert.alert("Participante existe", "Já existe um participante na lista com esse nome.");
     }
+
+    setParticipants(prevState => [...prevState, participantName]);
+    setParticipantName('')
   }
 
   function handleParticipantRemove(name: string) {
@@ -42,6 +49,8 @@ export function Home() {
                 style={styles.input}
                 placeholder='Nome do participante'
                 placeholderTextColor="#6B6B6B"
+                value={participantName}
+                onChangeText={setParticipantName}
             />
 
             <TouchableOpacity
